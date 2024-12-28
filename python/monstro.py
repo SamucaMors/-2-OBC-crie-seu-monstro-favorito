511389ddc8f0329e82029cc5f7bdd20c75c55897
SamucaMors <samuca.mors@gmail.com>
2024-12-28 20:59:54 -0300

    Adiciona implementação em Python

---
+++ python/monstro.py
@@ -0,0 +1,6 @@
+nome_monstro = input("Digite o nome do monstro: ")
+tipo_monstro = input("Digite o tipo do monstro: ")
+nivel_forca = int(input("Digite o nível de força do monstro (1 a 100): "))
+habilidade_especial = input("Digite a habilidade especial do monstro: ")
+
+print(f"O monstro {nome_monstro} é um {tipo_monstro} de nível {nivel_forca} e possui a habilidade especial: {habilidade_especial}")
